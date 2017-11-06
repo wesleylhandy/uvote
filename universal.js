2630bd70adb4aca0fa0dee4f1d7cd57b94043690
wesleylhandy <wesleylhandy@gmail.com>
2017-11-06 16:33:25 -0500

    debug universal.js

--- universal.js
+++ universal.js
@@ -10,8 +10,10 @@ const { default: App } = require('./client/src/App');
 require('./config/middleware');
 
 module.exports = function universalLoader(req, res) {
-    const filePath = path.resolve(__dirname, '..', 'build', 'index.html');
+    const filePath = path.resolve(__dirname, 'client', 'build', 'index.html');
+    
     console.log(filePath);
+
     fs.readFile(filePath, 'utf8', (err, htmlData) => {
         if (err) {
             console.error('read err', err)
