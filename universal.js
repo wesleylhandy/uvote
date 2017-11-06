4628b418e1ef2e24a7f7f5bb8937d8b8b7ecb3e0
wesleylhandy <wesleylhandy@gmail.com>
2017-11-06 16:41:50 -0500

    debug universal.js

--- universal.js
+++ universal.js
@@ -3,14 +3,15 @@ const path = require('path');
 const React = require('react');
 
 const { renderToString } = require('react-dom/server')
-const { StaticRouter, matchPath } = require('react-router-dom')
+const { StaticRouter } = require('react-router-dom')
 
-const { default: App } = require('./client/src/App');
+const { App } = require('./client/src/App');
 
-require('./config/middleware');
+require('./config/middleware.js');
 
 module.exports = function universalLoader(req, res) {
-    const filePath = path.resolve(__dirname, 'client', 'build', 'index.html');
+    
+    const filePath = path.join(__dirname, 'client/build/index.html');
     
     console.log(filePath);
 
@@ -22,9 +23,6 @@ module.exports = function universalLoader(req, res) {
         const context={};
         console.log('success reading file');
 
-        /*const match = matchPath(req.url, {
-            path: '/polls/single/:id/:title'
-        });*/
         let user = '', isAuth = false;
         if (req.isAuthenticated()) {
             user = req.user, isAuth = !req.session.guest;
@@ -32,14 +30,11 @@ module.exports = function universalLoader(req, res) {
             user = req.session.username, isAuth = !req.session.guest;
         }
 
-        const markup = renderToString( 
-            <StaticRouter location = { req.url } context={ context }>
-                <App userId={ user } isAuth={ isAuth }/>
-            </StaticRouter>)
+        const markup = renderToString(<StaticRouter location = { req.url } context={ context }><App userId={ user } isAuth={ isAuth }/></StaticRouter>)
 
         if (context.url) {
             // Somewhere a `<Redirect>` was rendered
-            res.redirect(301, context.url)
+            res.redirect(301, context.url);
         } else {
             // we're good, send the response
             const RenderedApp = htmlData.replace('{{SSR}}', markup);
