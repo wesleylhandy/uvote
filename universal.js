c3c8f43d66fb7e3e82422ec2030cd688e52c14a3
wesleylhandy <wesleylhandy@gmail.com>
2017-11-07 16:57:03 -0500

    --amend

--- universal.js
+++ universal.js
@@ -2,35 +2,40 @@ import fs from 'fs'
 import path from 'path'
 import React from 'react';
 
-import { renderToString }  from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 
-import App from './client/src/App.js';
+import App from './client/lib/App.js';
 
-module.exports = function universalLoader (req, res) {
+module.exports = function universalLoader(req, res) {
 
     const filePath = path.join(__dirname, 'client/build/index.html');
-    
+
     fs.readFile(filePath, 'utf8', (err, htmlData) => {
         if (err) {
             console.error('read err', err)
             return res.status(404).end()
         }
-        const context={};
+        const context = {};
 
-        let user = '', isAuth = false;
+        let user = '',
+            isAuth = false;
         if (req.isAuthenticated()) {
             user = req.user, isAuth = true;
         } else {
             user = req.session.username, isAuth = false;
         }
 
-        console.log({App});
+        console.log({ App });
 
-        const markup = renderToString(
-            <StaticRouter location={req.url} context={context}>
-                <App userId={user} isAuth={isAuth}/>
-            </StaticRouter>
+        const markup = renderToString( <
+            StaticRouter location = { req.url }
+            context = { context } >
+            <
+            App userId = { user }
+            isAuth = { isAuth }
+            /> <
+            /StaticRouter>
         );
 
         if (context.url) {
