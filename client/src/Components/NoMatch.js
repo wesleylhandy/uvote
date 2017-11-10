cec82097bbbe8b64e6bbc705fb51282b5dc30b75
Wesley Handy <wesleylhandy@gmail.com>
2017-11-10 09:08:18 -0500

    updated nomatch 404 component

--- client/src/Components/NoMatch.js
+++ client/src/Components/NoMatch.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 export default class NoMatch extends Component{
+	constructor(props){
+		super(props);
+	}
   render() {
     return <div className="no-match">The page you are looking for at {this.props.location.path} does not exist.</div>
   }
